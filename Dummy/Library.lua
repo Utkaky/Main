ada516e670a763ce2f339d2c93b8a365da47e2e7
Utka <toma0lele@gmail.com>
2026-03-02 23:12:40 -0300

    Update Library.lua

--- Dummy/Library.lua
+++ Dummy/Library.lua
@@ -205,7 +205,7 @@ do
 		end
 	end
 
-	local IconList = loadstring(game:HttpGet('https://raw.githubusercontent.com/Dummyrme/Library/refs/heads/main/Icon.lua'))()
+	local IconList = loadstring(game:HttpGet('https://raw.githubusercontent.com/Utkaky/Main/refs/heads/main/Dummy/Assets'))()
 	function gl(i)
 		local iconData = IconList.Icons[i]
 		if iconData then
